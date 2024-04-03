4c1dc56c8bb2e043fd3eff8ac7b06995b3e0f6dc
mohitrawat333 <151385966+mohitrawat333@users.noreply.github.com>
2024-04-03 20:50:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function getFormvalue() {
-    //Write your code herefunction getFormvalue() {
+    function getFormvalue() {
     // Get the values of first name and last name from the form
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
 
+    // Split the last name to handle cases with middle names
+    var lastNameParts = lastName.split(" ");
+    var lastNameToDisplay = lastNameParts.join(" ");
+
     // Combine first name and last name and display in an alert
-    var fullName = firstName + " " + lastName;
+    var fullName = firstName + " " + lastNameToDisplay;
     alert(fullName);
 }
 
-}
